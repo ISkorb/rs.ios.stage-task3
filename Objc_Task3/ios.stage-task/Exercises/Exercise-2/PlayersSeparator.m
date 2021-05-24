8cf9e895a75dac0b017a04d9e2231bfedb3bfec4
ISkorb <ina.skorb@gmail.com>
2021-05-24 16:19:59 +0300

    feat: add task2 implementation

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -2,7 +2,49 @@
 
 @implementation PlayersSeparator
 
-- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
+- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray
+{
+    @autoreleasepool
+    {
+        if(ratingArray == nil || ratingArray.count < 3)
+        {
+            return 0;
+        }
+        
+        int arrayOfInt[ratingArray.count];
+                    
+        for (int i = 0; i < ratingArray.count; i++)
+        {
+            int currentValue = [(NSNumber *)[ratingArray objectAtIndex:i] intValue];
+            arrayOfInt[i] = currentValue;
+        }
+         
+        int n = (int)(sizeof(arrayOfInt) / sizeof(arrayOfInt[0]));
+        int inversionCount = 0;
+        int conversionCount = 0;
+         
+        for (int i = 0; i < n - 2; i++)
+        {
+            for (int j = i + 1; j < n - 1; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    if (arrayOfInt[i] > arrayOfInt[j] && arrayOfInt[j] > arrayOfInt[k])
+                    {
+                        inversionCount++;
+                    }
+                    
+                    if (arrayOfInt[i] < arrayOfInt[j] && arrayOfInt[j] < arrayOfInt[k])
+                    {
+                        conversionCount++;
+                    }
+                }
+            }
+        }
+        
+        return (inversionCount + conversionCount);
+    }
+    
     return 0;
 }
 
