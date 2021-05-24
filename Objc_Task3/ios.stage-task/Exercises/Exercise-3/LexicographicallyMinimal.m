2124321fcec4e3c5cb252fa6aabb5897b7c5f02b
ISkorb <ina.skorb@gmail.com>
2021-05-24 20:47:05 +0300

    feat: add task3 temporary implementation. Need to be fixed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -8,7 +8,37 @@
 
 @implementation LexicographicallyMinimal
 
--(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
+-(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2
+{
+    @autoreleasepool
+    {
+        if(string1.length == 0 || string2.length == 0)
+        {
+            return nil;
+        }
+       
+        NSString* concat = [string1 stringByAppendingString:string2];
+        NSMutableArray* letterArray = [[NSMutableArray alloc]init];
+        
+        for(int i = 0; i < concat.length; i++)
+        {
+            NSRange range = NSMakeRange(i, 1);
+            NSString* character = [concat substringWithRange:range];
+            [letterArray addObject:character];
+        }
+        
+        id mySort = ^(NSString * a, NSString * b)
+        {
+            NSString* ab = [a stringByAppendingString:b];
+            NSString* ba = [b stringByAppendingString:a];
+            
+            return [ab compare:ba];
+        };
+        
+        NSArray* sortedArray = [letterArray sortedArrayUsingComparator:mySort];
+        NSString* joinedString = [sortedArray componentsJoinedByString:@""];
+        return joinedString;
+    }
     return nil;
 }
 
